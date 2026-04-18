0fc88a323fa635ea7b35797ff5a510e409ec6d7a
Vitória Bentes <73360746+vitoriabentes@users.noreply.github.com>
2026-04-18 12:02:23 -0300

    Incluído spring boot com api rest padrão para testar deploy em AWS

---
+++ negociacao-ativos/src/main/java/com/negociacaoativos/controller/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.negociacaoativos.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+}
