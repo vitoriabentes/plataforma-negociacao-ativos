fa0f5a26344ba8660023ef1b66088f3f3c9eda0e
le-affes <leandro.arcanjo@usp.br>
2026-04-21 20:12:26 -0300

    Removido PlataformaNegociacaoAtivosApplication

--- src/main/java/com/negociacaoativos/PlataformaNegociacaoAtivosApplication.java
+++
@@ -1,7 +0,0 @@
-@SpringBootApplication
-public class PlataformaNegociacaoAtivosApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(PlataformaNegociacaoAtivosApplication.class, args);
-    }
-}
