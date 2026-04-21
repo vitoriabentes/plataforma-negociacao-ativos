5e14f24becd22b71631c65c4d2a75a4425257269
le-affes <leandro.arcanjo@usp.br>
2026-04-21 19:57:32 -0300

    Adicionada PlataformaNegociacaoAtivosApplication

---
+++ src/src/main/java/com/negociacaoativos/PlataformaNegociacaoAtivosApplication.java
@@ -0,0 +1,7 @@
+@SpringBootApplication
+public class PlataformaNegociacaoAtivosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlataformaNegociacaoAtivosApplication.class, args);
+    }
+}
