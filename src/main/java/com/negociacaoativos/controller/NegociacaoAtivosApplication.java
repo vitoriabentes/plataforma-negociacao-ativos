2fb85cfb6f8691553f1b18201657abc49143e740
Vitória Bentes <73360746+vitoriabentes@users.noreply.github.com>
2026-04-22 23:41:48 -0300

    Incluído main no projeto

---
+++ src/main/java/com/negociacaoativos/controller/NegociacaoAtivosApplication.java
@@ -0,0 +1,13 @@
+package com.negociacaoativos.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NegociacaoAtivosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NegociacaoAtivosApplication.class, args);
+    }
+
+}
